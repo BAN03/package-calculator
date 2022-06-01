ff2ce80944610ac4af0374d7a1ac30799ffeb74b
BAN03 <angeldejesusatanasiodiaz@gmail.com>
2022-06-01 12:09:33 -0500

    delete

--- main.py
+++
@@ -1,6 +0,0 @@
-import imp
-from calculadora import suma
-if __name__ == "__main__":
-    numero_1 = int(input("Primer numero: "))
-    numero_2 = int(input("Segundo numero: "))
-    suma(numero_1, numero_2)
