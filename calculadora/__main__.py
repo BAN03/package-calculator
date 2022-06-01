6c9d5db83bd9d933dee6b5accf20cb0a52ea1730
BAN03 <angeldejesusatanasiodiaz@gmail.com>
2022-06-01 12:26:52 -0500

    seleccion de operacion

--- calculadora/__main__.py
+++ calculadora/__main__.py
@@ -1,5 +1,20 @@
-from calculadora import suma
+import calculadora
 if __name__ == "__main__":
-    numero_1 = int(input("Primer numero: "))
-    numero_2 = int(input("Segundo numero: "))
-    suma(numero_1, numero_2)
+
+    print("Selecciona una operacion:\n1. Suma\n2. Resta\n3. Multiplicacion\n4. Division")
+    opcion = int(input("Opcion: "))
+
+    if not opcion > 4:
+        numero_1 = int(input("Primer numero: "))
+        numero_2 = int(input("Segundo numero: "))
+    
+        if opcion == 1:
+            calculadora.suma(numero_1, numero_2)
+        elif opcion == 2:
+            calculadora.resta(numero_1, numero_2)
+        elif opcion == 3:
+            calculadora.multiplicacion(numero_1, numero_2)
+        elif opcion == 4:
+            calculadora.division(numero_1, numero_2)
+    else:
+        print("Opcion Incorrecta")
