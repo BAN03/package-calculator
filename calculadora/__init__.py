4cf828064bb17089f55ca8e9c371d7aadeb0d0c1
BAN03 <angeldejesusatanasiodiaz@gmail.com>
2022-06-01 02:12:46 -0500

    First Commit

---
+++ calculadora/__init__.py
@@ -0,0 +1,4 @@
+def suma():
+    numero_1 = int(input("Primer numero: "))
+    numero_2 = int(input("Segundo numero: "))
+    print(f"El resultado es: {numero_1 + numero_2}")
---
+++ main.py
@@ -0,0 +1,3 @@
+from calculadora import suma as sum
+if __name__ == "__main__":
+    pass
